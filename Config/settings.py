4d2db2f55519bace2f848fc3b5e5f719cba4ede2
Vira <guidevirgate@gmail.com>
2026-02-09 01:26:26 +0500

    sssasac

--- Config/settings.py
+++ Config/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&#j5cdw+4(rtz=k$l3edbele%h^7@9pgeo@cxl(7u7+1&fx!c5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # В settings.py
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 #fs
 
 INSTALLED_APPS = [
