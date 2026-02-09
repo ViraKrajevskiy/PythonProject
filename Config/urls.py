dc3570591094e8a86fd05fd20dc022364fa7f542
Vira <156062320+ViraKrajevskiy@users.noreply.github.com>
2026-02-09 21:40:50 +0500

    админ

--- Config/urls.py
+++ Config/urls.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+from django.contrib.auth import get_user_model
+
+def create_admin(request):
+    try:
+        User = get_user_model()
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser('admin', 'admin@example.com', '3003')
+            return HttpResponse("Админ создан!")
+        return HttpResponse("Уже есть.")
+    except Exception as e:
+        return HttpResponse(f"Ошибка: {e}")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('init-admin/', create_admin),
     path('', include('Project.urls')), # Подключаем ссылки приложения
 ]
